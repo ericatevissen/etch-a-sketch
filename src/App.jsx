3e402dfd538a729b5e514368ef7e27f82a23c6f1
ericaofthewired <ericatevissen@proton.me>
2023-11-27 01:10:01 +0000

    implemented drawing and resizing

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,78 @@ import { useState } from 'react'
 import './App.css'
 
 export default function App() {
+  const [canvasSize, setCanvasSize] = useState(16);
+  const [pixelColors, setPixelColors] = useState(Array(canvasSize * canvasSize).fill("white"));
+
+  function handleResize() {
+    let sizeInput = prompt("Size? Max: 64")
+    if (sizeInput > 64) sizeInput = 64;
+    setCanvasSize(sizeInput);
+    setPixelColors(Array(canvasSize * canvasSize).fill("white"));
+  }
+
+  function handleHover(index) {
+    const updatedPixelColors = [...pixelColors];
+    updatedPixelColors[index] = "black";
+    setPixelColors(updatedPixelColors);
+  }
+
   return (
     <>
       <h1>Etch a Sketch</h1>
       <div className="container">
         <div className="controls">
-          <button>Clean</button>
-          <button>Change size</button>
+          <CleanButton />
+          <ResizeButton handleResize={handleResize}/>  
         </div>
-        <div className="canvas" />
+        <Canvas canvasSize={canvasSize} pixelColors={pixelColors} handleHover={handleHover}/> 
       </div>
     </>
-  )
+  );
 }
+
+function CleanButton() {
+  return (
+    <button>Clean</button>
+  );
+}
+
+function ResizeButton({ handleResize }) {
+  return (
+    <button onClick={handleResize}>Change size</button>
+  );
+}
+
+function Canvas({ canvasSize, pixelColors, handleHover }) {
+
+  const pixels = drawCanvas(canvasSize, pixelColors, handleHover);
+
+  return (
+    <div className="canvas">
+      {pixels}
+    </div>
+  );
+}
+
+function drawCanvas(canvasSize, pixelColors, handleHover) {
+  const pixels = [];
+
+  for (let i = 0; i < canvasSize * canvasSize; i++) {
+    pixels.push(
+    <div 
+      key={i} 
+      style={{
+        width: 512 / canvasSize + "px", 
+        height: 512 / canvasSize + "px",
+        backgroundColor: pixelColors[i]
+      }}
+      className='pixel'
+      onMouseOver={() => handleHover(i)}
+    >
+    </div>
+    );
+  }
+
+  return pixels;
+}
+
